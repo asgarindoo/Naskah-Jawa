5413a413fe8981864b4eb13ede73bcc455d9ab71
Asgarindo Dwiki Ibrahim adji <asgarindo9@gmail.com>
2024-01-05 12:39:37 +0700

    add modules

---
+++ If Statement/if-else.js
@@ -0,0 +1,11 @@
+let question = prompt(
+  "Are you having trouble learning JavaScript? (yes or no)"
+).toLowerCase();
+
+if (question === "yes") {
+  alert("YOU GAY");
+} else if (question === "no") {
+  alert("Good job!");
+} else {
+  alert("ACTUALLY YOU GAY");
+}
